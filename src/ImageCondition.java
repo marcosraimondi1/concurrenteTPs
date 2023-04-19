d981f7d120399ccc29ab2d6913cc8a5389b8f318
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-04-18 22:51:39 -0300

    doc ImageCondition

--- src/ImageCondition.java
+++ src/ImageCondition.java
@@ -1,3 +1,9 @@
+/**
+ * Interfaz Funcional para crear condiciones a cumplir
+ * por una imagen a buscar.
+ * Ej de uso:
+ * ImageCondition condicion = (Imagen imagen) -> { if (imagen.isAjustada()) return imagen; }
+ */
 public interface ImageCondition {
     Imagen verificar(Imagen imagen);
 }
