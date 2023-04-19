8644e8afb7c2ff5e7b5d5fca6f53534f9a552fd2
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-04-18 22:12:16 -0300

    Imagen object, metodos y campos principales

--- src/Imagen.java
+++ src/Imagen.java
@@ -1,2 +1,29 @@
 public class Imagen {
+    private final int id;
+    private int improvements = 0;
+    private boolean ajustada = false;
+
+    public Imagen(int id) {
+        this.id = id;
+    }
+
+    public void improve() {
+        improvements += 1;
+    }
+
+    public boolean isImproved() {
+        return improvements == 3;
+    }
+
+    public void ajustar() {
+        ajustada = true;
+    }
+
+    public boolean isAjustada() {
+        return ajustada;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
