cc5196a3fcfc9a6fd12369e3da281613dd6c38d0
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-04-18 23:12:59 -0300

    algunas pruebas de buscqueda de imagenes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        Contenedor contenedor = new Contenedor(100);
+        for (int i = 0; i < 100; i++) {
+            Imagen imagen = new Imagen(i);
+            if (i == 10) imagen.ajustar();
 
+            contenedor.addImage(imagen);
+        }
+
+        ImageCondition ajustada = Imagen::isAjustada;
+
+        Imagen image = contenedor.getImage(ajustada);
+        System.out.println(image.getId());
     }
 }
