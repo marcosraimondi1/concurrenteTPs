9238ed6fd4593d7620b9e102ef190f64e9f40ea6
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-04-18 22:46:40 -0300

    metodo para buscar imagen con condicion

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -16,6 +16,16 @@ public class Contenedor {
         return contenedor.remove(0);
     }
 
+    /**
+     * Busca una imagen que satisfaga la condicion dada
+     *
+     * @param condicion Condicion a satisfacer por la imagen buscada
+     * @return Imagen que satisface la condicion
+     */
+    public Imagen getImage(ImageCondition condicion) {
+        return contenedor.stream().map(imagen -> condicion.verificar(imagen)).findFirst().get();
+    }
+
     public void addImage(Imagen imagen) {
         contenedor.add(imagen);
     }
---
+++ src/ImageCondition.java
@@ -0,0 +1,3 @@
+public interface ImageCondition {
+    Imagen verificar(Imagen imagen);
+}
