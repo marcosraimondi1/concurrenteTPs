9fdbd2f77692281236b9f074e9db08822e078b48
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-04-18 22:44:49 -0300

    Metodos y campos principales de Contenedor, SIN SINCRONIZACION

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Contenedor {
+    private final int maxSize;
+    private ArrayList<Imagen> contenedor;
+
+    public Contenedor(int maxSize) {
+        this.maxSize = maxSize;
+        contenedor = new ArrayList<Imagen>();
+    }
+
+    /**
+     * Busca una imagen
+     */
+    public Imagen getImage() {
+        return contenedor.remove(0);
+    }
+
+    public void addImage(Imagen imagen) {
+        contenedor.add(imagen);
+    }
+
+    public boolean isEmpty() {
+        return contenedor.isEmpty();
+    }
+
+    public boolean isFull() {
+        return contenedor.size() == maxSize;
+    }
 }
