d420bf8569246bb3741db9cb35c7167045872ee3
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-06-23 21:11:19 -0300

    cola tests

---
+++ tests/Cola/ColaTest.java
@@ -0,0 +1,67 @@
+package Cola;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColaTest {
+
+    @Test
+    void hayEsperando() {
+        Cola cola = new Cola();
+        assertFalse(cola.hayEsperando());
+        Thread thread = new Thread(cola::hacerCola);
+        thread.start();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertTrue(cola.hayEsperando());
+
+        cola.sacar();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertFalse(cola.hayEsperando());
+
+    }
+
+    @Test
+    void hacerColaySacar() {
+        Cola cola = new Cola();
+        AtomicBoolean variable = new AtomicBoolean(false);
+        Thread thread = new Thread(()->{
+            cola.hacerCola();
+            variable.set(true);
+        });
+        thread.start();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertFalse(variable.get());
+
+        cola.sacar();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertTrue(variable.get());
+    }
+
+}
