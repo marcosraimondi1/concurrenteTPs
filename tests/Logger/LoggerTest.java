3b5a1e857ce7155ef51eb63afafd7fdd57df0400
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-06-27 18:16:44 -0300

    loggerTest funciona

--- tests/Logger/LoggerTest.java
+++ tests/Logger/LoggerTest.java
@@ -2,14 +2,29 @@ package Logger;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoggerTest {
 
     @Test
     void log() {
+        String s = "T1";
+        String s2 = "T2";
         Logger logger = new Logger(".\\data\\log.txt");
-        logger.log("Hello World");
+        logger.log(s);
+        logger.log(s2);
+        File file = new File(".\\data\\log.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            assertEquals(s+s2, line);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
